4071fe6002983a907113c3cf4d0029bacf235540
Yau1heni <lodbrok9@gmail.com>
2025-02-10 08:00:44 +0300

    feat: add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rick-and-morty',
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   }
